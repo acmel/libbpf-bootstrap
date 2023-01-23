87f7cf0d709f6de17fdbb1564f1e852d4f80c138
Arnaldo Carvalho de Melo <acme@redhat.com>
2023-01-23 17:15:21 -0300

    mallocsnoop.bpf.c: Use pid + addr as the key for the malloc chunks
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- examples/c/mallocsnoop.bpf.c
+++ examples/c/mallocsnoop.bpf.c
@@ -20,6 +20,18 @@ struct {
 	__type(value, struct malloc_entry);
 } malloc_start SEC(".maps");
 
+struct malloc_addrs_key {
+	u64  pid; // Not pid_t (32 bits) to avoid a struct alignment hole, the verifier will refuse loading in that case.
+	void *addr;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 65536);
+	__type(key, struct malloc_addrs_key);
+	__type(value, struct malloc_entry);
+} malloc_addrs SEC(".maps");
+
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
 	__uint(max_entries, 256 * 1024);
@@ -56,14 +68,26 @@ int BPF_URETPROBE(malloc_out, void *addr) // addr returned by malloc
 	if (pid == my_pid)
 		return 0;
 
+	struct malloc_entry *entry = bpf_map_lookup_elem(&malloc_start, &pid);
+
+	if (!entry)
+		return 0;
+
+	struct malloc_addrs_key addrs_key = {
+		.addr = addr,
+		.pid  = pid,
+	};
+	struct malloc_entry chunk = {
+		.size = entry->size,
+		.ts   = entry->ts,
+	};
+
+	bpf_map_update_elem(&malloc_addrs, &addrs_key, &chunk, BPF_ANY);
+	bpf_map_delete_elem(&malloc_start, &pid);
+
         /* don't emit malloc events when minimum duration is specified */
         if (min_duration_ns)
                 return 0;
-
-	struct malloc_entry *entry = bpf_map_lookup_elem(&malloc_start, &pid);
-
-	if (entry == NULL)
-		return 0;
 
 	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
 	if (!e)
@@ -72,7 +96,7 @@ int BPF_URETPROBE(malloc_out, void *addr) // addr returned by malloc
 	e->free_event = false;
 	e->pid = pid;
 	e->addr = addr;
-	e->size = entry->size;
+	e->size = chunk.size;
 	bpf_get_current_comm(&e->comm, sizeof(e->comm));
 
 	/* successfully submit it to user-space for post-processing */
@@ -87,13 +111,16 @@ int BPF_UPROBE(handle_free, void *addr)
 
 	if (pid == my_pid)
 		return 0;
+	struct malloc_addrs_key addrs_key = {
+		.addr = addr,
+		.pid  = pid,
+	};
+	struct malloc_entry *chunk = bpf_map_lookup_elem(&malloc_addrs, &addrs_key);
 
-	struct malloc_entry *entry = bpf_map_lookup_elem(&malloc_start, &pid);
-
-	if (entry == NULL)
+	if (chunk == NULL)
 		return 0;
 
-	u64 duration_ns = bpf_ktime_get_ns() - entry->ts;
+	u64 duration_ns = bpf_ktime_get_ns() - chunk->ts;
 
         if (min_duration_ns && duration_ns < min_duration_ns)
                 return 0;
@@ -105,10 +132,11 @@ int BPF_UPROBE(handle_free, void *addr)
 	e->free_event = true;
 	e->pid = pid;
 	e->addr = addr;
+	e->size = chunk->size;
 	e->duration_ns = duration_ns;
 	bpf_get_current_comm(&e->comm, sizeof(e->comm));
 
-	bpf_map_delete_elem(&malloc_start, &pid);
+	bpf_map_delete_elem(&malloc_addrs, &addrs_key);
 
 	/* send data to user-space for post-processing */
 	bpf_ringbuf_submit(e, 0);
